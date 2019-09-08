162a44c931f71beefbbe54824fad0d9207356259
Serra Allgood <serra@allgood.dev>
2019-09-08 14:06:44 -0700

    Add commented instructions for asdf users

--- start.sh
+++ start.sh
@@ -3,6 +3,12 @@
 MYDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $MYDIR
 
+# If you use asdf for language version management,
+# node won't be in the path of the user process
+# unless you uncomment the following lines
+#export PATH=$PATH:/usr/local/bin:/usr/local:/bin
+#source $HOME/.asdf/asdf.sh
+
 while true; do
     DEBUG=*matrix-puppet:* node index.js
     sleep 1
